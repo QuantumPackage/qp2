f182626685d5937ede71e75106652eea13cedc31
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2020-04-23 23:18:12 +0200

    Fixed Forcing kill message

--- src/zmq/utils.irp.f
+++ src/zmq/utils.irp.f
@@ -589,11 +589,11 @@ subroutine end_parallel_job(zmq_to_qp_run_socket,zmq_socket_pull,name_in)
     rc = f77_zmq_send(zmq_to_qp_run_socket, 'end_job '//trim(zmq_state),8+len(trim(zmq_state)),0)
     rc = f77_zmq_recv(zmq_to_qp_run_socket, message, 512, 0)
     if (trim(message(1:13)) == 'error waiting') then
+      call sleep(1)
       cycle
     else if (message(1:2) == 'ok') then
       exit
     endif
-    call sleep(1)
   end do
   if (i==0) then
     print *,  '.. Forcing kill ..'
