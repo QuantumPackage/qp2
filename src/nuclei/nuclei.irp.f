9c52a612dd514ab655653969c993e0cc2a501b1f
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2020-05-25 09:18:31 +0200

    Interatomic distance

--- src/nuclei/nuclei.irp.f
+++ src/nuclei/nuclei.irp.f
@@ -42,6 +42,7 @@ BEGIN_PROVIDER [ double precision, nucl_coord,  (nucl_num,3) ]
          '     Atom          Charge          X            Y            Z '
      write(6,ft)                                         &
          '================','============','============','============','============'
+
      do i=1,nucl_num
        write(6,f) nucl_label(i), nucl_charge(i),         &
            nucl_coord(i,1)*a0,                                       &
@@ -51,6 +52,19 @@ BEGIN_PROVIDER [ double precision, nucl_coord,  (nucl_num,3) ]
      write(6,ft)                                         &
          '================','============','============','============','============'
      write(6,'(A)') ''
+
+     double precision               :: dist_min, x, y, z
+     dist_min = huge(1.d0)
+     do i=1,nucl_num
+       do j=i+1,nucl_num
+         x = nucl_coord(i,1)-nucl_coord(j,1)
+         y = nucl_coord(i,2)-nucl_coord(j,2)
+         z = nucl_coord(i,3)-nucl_coord(j,3)
+         dist_min = min(x*x + y*y + z*z, dist_min)
+       enddo
+     enddo
+     write(6,'(A,F12.4,A)') 'Minimal interatomic distance found: ', &
+         dsqrt(dist_min)*a0,' Angstrom'
 
    endif
 
