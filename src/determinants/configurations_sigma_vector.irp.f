0f9c1e6cb3523a52cb8f19ea6843f82efa113e65
v1j4y <vijay.gopal.c@gmail.com>
2021-01-28 02:14:59 +0100

    Working on building the prototype matrix AIJpq #143.

---
+++ src/determinants/configurations_sigma_vector.irp.f
@@ -0,0 +1,10 @@
+  BEGIN_PROIDER[ integer, NSOMOMax]
+  implicit none
+  BEGIN_DOC
+  ! Documentation for NSOMOMax
+  ! The maximum number of SOMOs for the current calculation.
+  ! required for the calculation of prototype arrays.
+  END_DOC
+  NSOMOMax = 8
+  NCSFMAx = 14 ! TODO: NCSFs for MS=0
+  END_PROVIDER
