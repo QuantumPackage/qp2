QuantumPackage/qp2

commit 0bf0513fb13623d3a2807c11a8316eea51a72705

Added VERSION file

src/ao_basis/aos.irp.f
@@ -40,9 +40,9 @@ END_PROVIDER
 
   do i=1,ao_num
 
-    powA(1) = ao_power(i,1)
-    powA(2) = ao_power(i,2)
-    powA(3) = ao_power(i,3)
+    powA(1) = ao_power(i,1) +  ao_power(i,2) +  ao_power(i,3)
+    powA(2) = 0
+    powA(3) = 0
 
     ! Normalization of the primitives
     if (primitives_normalized) then
@@ -56,6 +56,10 @@ END_PROVIDER
         ao_coef_normalized(i,j) = ao_coef(i,j)
       enddo
     endif
+
+    powA(1) = ao_power(i,1)
+    powA(2) = ao_power(i,2)
+    powA(3) = ao_power(i,3)
 
     ! Normalization of the contracted basis functions
     if (ao_normalized) then

VERSION
@@ -1,0 +1,1 @@
+2.2.1

ocaml/qp_create_ezfio.ml
@@ -604,9 +604,30 @@ let run ?o b au c d m p cart xyz_file =
           ~rank:1 ~dim:[| shell_num |] ~data:ang_mom ) ;
         Ezfio.set_basis_shell_prim_index (Ezfio.ezfio_array_of_list
           ~rank:1 ~dim:[| shell_num |] ~data:shell_prim_idx) ;
-        Ezfio.set_basis_shell_nucl (Ezfio.ezfio_array_of_list
-          ~rank:1 ~dim:[| shell_num |]
-          ~data:(list_map (fun (_,n) -> Nucl_number.to_int n) basis) ) ;
+        Ezfio.set_basis_basis_nucleus_index (Ezfio.ezfio_array_of_list
+          ~rank:1 ~dim:[| nucl_num |]
+          ~data:(
+          list_map (fun (_,n) -> Nucl_number.to_int n) basis
+          |> List.fold_left (fun accu i -> 
+            match accu with 
+            | [] -> []
+            | (h,j) :: rest -> if j == i then ((h+1,j)::rest) else ((h+1,i)::(h+1,j)::rest)
+          ) [(0,0)]
+          |> List.rev
+          |> List.map fst
+          )) ;
+        Ezfio.set_basis_nucleus_shell_num(Ezfio.ezfio_array_of_list
+          ~rank:1 ~dim:[| nucl_num |]
+          ~data:(
+          list_map (fun (_,n) -> Nucl_number.to_int n) basis
+          |> List.fold_left (fun accu i -> 
+            match accu with 
+            | [] -> [(1,i)]
+            | (h,j) :: rest -> if j == i then ((h+1,j)::rest) else ((1,i)::(h,j)::rest)
+          ) []
+          |> List.rev
+          |> List.map fst
+          )) ;
         Ezfio.set_basis_prim_coef (Ezfio.ezfio_array_of_list
           ~rank:1 ~dim:[| prim_num |] ~data:coef) ;
         Ezfio.set_basis_prim_expo (Ezfio.ezfio_array_of_list

src/basis/EZFIO.cfg
@@ -13,6 +13,12 @@ type: integer
 doc: Number of shells
 interface: ezfio, provider
 
+[nucleus_shell_num]
+type: integer
+doc: Number of shells per nucleus
+size: (nuclei.nucl_num)
+interface: ezfio, provider
+
 [shell_normalization_factor]
 type: double precision
 doc: Normalization factor applied to the whole shell, ex $1/\sqrt{ <d_{z^2}|d_{z^2}>}$
@@ -37,10 +43,10 @@ doc: Max number of primitives in a shell
 size: (basis.shell_num)
 interface: ezfio, provider
 
-[shell_nucl]
+[basis_nucleus_index]
 type: integer
 doc: Index of the nucleus on which the shell is centered
-size: (basis.shell_num)
+size: (nuclei.nucl_num)
 interface: ezfio, provider
 
 [prim_normalization_factor]

commit 64003ba07de2c43645ed4a12ed6023470ada3935

Fixed bug introduced by 0bf0513fb136

src/ao_basis/aos.irp.f
@@ -40,6 +40,9 @@ END_PROVIDER
 
   do i=1,ao_num
 
+!    powA(1) = ao_power(i,1) +  ao_power(i,2) +  ao_power(i,3)
+!    powA(2) = 0
+!    powA(3) = 0
     powA(1) = ao_power(i,1)
     powA(2) = ao_power(i,2)
     powA(3) = ao_power(i,3)
@@ -49,7 +52,7 @@ END_PROVIDER
       do j=1,ao_prim_num(i)
         call overlap_gaussian_xyz(C_A,C_A,ao_expo(i,j),ao_expo(i,j), &
            powA,powA,overlap_x,overlap_y,overlap_z,norm,nz)
-        ao_coef_normalized(i,j) = ao_coef(i,j)/sqrt(norm)
+        ao_coef_normalized(i,j) = ao_coef(i,j)/dsqrt(norm)
       enddo
     else
       do j=1,ao_prim_num(i)
@@ -66,7 +69,7 @@ END_PROVIDER
           norm = norm+c*ao_coef_normalized(i,j)*ao_coef_normalized(i,k)
         enddo
       enddo
-      ao_coef_normalization_factor(i) = 1.d0/sqrt(norm)
+      ao_coef_normalization_factor(i) = 1.d0/dsqrt(norm)
     else
       ao_coef_normalization_factor(i) = 1.d0
     endif