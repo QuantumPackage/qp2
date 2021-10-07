0c7c8513b1c5ccb1a6bba46dcc75a5b4c6b59580
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-10-07 17:37:24 +0200

    added the possibility to have a mu(r) in the functionals

--- src/dft_one_e/mu_erf_dft.irp.f
+++ src/dft_one_e/mu_erf_dft.irp.f
@@ -8,3 +8,15 @@ BEGIN_PROVIDER [double precision, mu_erf_dft]
  mu_erf_dft = mu_erf
 
 END_PROVIDER
+
+BEGIN_PROVIDER [double precision, mu_of_r_dft, (n_points_final_grid)]
+ implicit none
+ integer :: i
+ do i = 1, n_points_final_grid
+  if(mu_dft_type == "cst")then
+   mu_of_r_dft(i) = mu_erf_dft
+  else
+   mu_of_r_dft(i) = mu_of_r_hf(i)
+  endif
+ enddo
+END_PROVIDER 
