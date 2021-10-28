b19569905057f36aedbd478620d57cf4f8b4542b
Emmanuel Giner <giner.emmanuel@gmail.com>
2021-10-28 15:19:45 +0200

    added H_matrix_diag_all_dets

--- src/determinants/utils.irp.f
+++ src/determinants/utils.irp.f
@@ -20,6 +20,25 @@ BEGIN_PROVIDER [ double precision, H_matrix_all_dets,(N_det,N_det) ]
  !$OMP END PARALLEL DO
 END_PROVIDER
 
+BEGIN_PROVIDER [ double precision, H_matrix_diag_all_dets,(N_det) ]
+  use bitmasks
+ implicit none
+ BEGIN_DOC
+ ! |H| matrix on the basis of the Slater determinants defined by psi_det
+ END_DOC
+ integer :: i
+ double precision :: hij
+ integer :: degree(N_det)
+ call  i_H_j(psi_det(1,1,1),psi_det(1,1,1),N_int,hij)
+ !$OMP PARALLEL DO SCHEDULE(GUIDED) DEFAULT(NONE) PRIVATE(i,hij,degree) &
+ !$OMP SHARED (N_det, psi_det, N_int,H_matrix_diag_all_dets)
+ do i =1,N_det
+   call  i_H_j(psi_det(1,1,i),psi_det(1,1,i),N_int,hij)
+   H_matrix_diag_all_dets(i) = hij
+ enddo
+ !$OMP END PARALLEL DO
+END_PROVIDER
+
 
 BEGIN_PROVIDER [ double precision, S2_matrix_all_dets,(N_det,N_det) ]
   use bitmasks
