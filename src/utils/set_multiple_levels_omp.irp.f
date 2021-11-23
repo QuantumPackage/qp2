082b32b24f5817ea2f5e217835701a4507677ef7
ydamour <yann.damour@hotmail.fr>
2021-11-23 10:33:37 +0100

    remove comments

--- src/utils/set_multiple_levels_omp.irp.f
+++ src/utils/set_multiple_levels_omp.irp.f
@@ -5,11 +5,11 @@ subroutine set_multiple_levels_omp()
   implicit none
 
   IRP_IF SET_MAX_ACT
-    print*,'SET_MAX_ACT: True, call omp_set_max_active_levels(5)'
+    !print*,'SET_MAX_ACT: True, call omp_set_max_active_levels(5)'
     call omp_set_max_active_levels(5)
   IRP_ENDIF
   IRP_IF SET_NESTED
-    print*,'SET_NESTED: True, call omp_set_nested(.True.)'
+    !print*,'SET_NESTED: True, call omp_set_nested(.True.)'
     call omp_set_nested(.True.)
   IRP_ENDIF
 
