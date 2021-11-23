7fcd03b911424770fcd833549f4e573569f83604
ydamour <yann.damour@hotmail.fr>
2021-11-23 15:00:54 +0100

    comments

--- scripts/verif_omp/check_actual_setup.sh
+++ scripts/verif_omp/check_actual_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# go in qp2/src/fci to run check_omp_actual_setup
+# to see if we can run in parallel an omp section in another one
 echo ""
 echo "Please wait..."
 echo ""
--- scripts/verif_omp/study_omp.sh
+++ scripts/verif_omp/study_omp.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
+# list of compilers
 list_comp="ifort gfortran-7 gfortran-8 gfortran-9"
 
+# file to store the results
 FILE=results.dat
 
 touch $FILE
 rm $FILE
 
+# Comments
 echo "1: omp_set_max_active_levels(5)" >> $FILE
 echo "2: omp_set_nested(.True.)" >> $FILE
 echo "3: 1 + 2" >> $FILE
 echo "" >> $FILE
 echo "1 2 3" >> $FILE
+
+# loop on the comp
 for comp in $list_comp
 do
 	$comp --version > /dev/null \
@@ -20,5 +25,6 @@ do
 
 done
 
+# Display
 cat $FILE
 
