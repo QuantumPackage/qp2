cf96b74b52443e63d20c8d414315299ce26591ad
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-03-16 10:26:18 +0100

    Remove debug

--- src/determinants/dipole_moments.irp.f
+++ src/determinants/dipole_moments.irp.f
@@ -9,7 +9,7 @@
  double precision :: weight, r(3)  
  double precision :: cpu0,cpu1,nuclei_part_z,nuclei_part_y,nuclei_part_x
 
- call cpu_time(cpu0)
+! call cpu_time(cpu0)
  z_dipole_moment = 0.d0
  y_dipole_moment = 0.d0
  x_dipole_moment = 0.d0
@@ -26,10 +26,10 @@
   enddo
  enddo
  
- print*,'electron part for z_dipole = ',z_dipole_moment
- print*,'electron part for y_dipole = ',y_dipole_moment
- print*,'electron part for x_dipole = ',x_dipole_moment
- 
+! print*,'electron part for z_dipole = ',z_dipole_moment
+! print*,'electron part for y_dipole = ',y_dipole_moment
+! print*,'electron part for x_dipole = ',x_dipole_moment
+! 
  nuclei_part_z = 0.d0
  nuclei_part_y = 0.d0
  nuclei_part_x = 0.d0
@@ -38,18 +38,18 @@
   nuclei_part_y += nucl_charge(i) * nucl_coord(i,2) 
   nuclei_part_x += nucl_charge(i) * nucl_coord(i,1) 
  enddo
- print*,'nuclei   part for z_dipole = ',nuclei_part_z
- print*,'nuclei   part for y_dipole = ',nuclei_part_y
- print*,'nuclei   part for x_dipole = ',nuclei_part_x
-
+! print*,'nuclei   part for z_dipole = ',nuclei_part_z
+! print*,'nuclei   part for y_dipole = ',nuclei_part_y
+! print*,'nuclei   part for x_dipole = ',nuclei_part_x
+!
  do istate = 1, N_states
   z_dipole_moment(istate) += nuclei_part_z
   y_dipole_moment(istate) += nuclei_part_y
   x_dipole_moment(istate) += nuclei_part_x
  enddo
 
- call cpu_time(cpu1)
- print*,'Time to provide the dipole moment :',cpu1-cpu0
+! call cpu_time(cpu1)
+! print*,'Time to provide the dipole moment :',cpu1-cpu0
 END_PROVIDER
 
 
