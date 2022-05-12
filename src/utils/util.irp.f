QuantumPackage/qp2

commit 57527c94a27491107e49d9623c0124c516976d34

update binom_func to avoid .999...

src/utils/util.irp.f
@@ -37,6 +37,10 @@ double precision function binom_func(i,j)
   else
     binom_func = dexp( logfact(i)-logfact(j)-logfact(i-j) )
   endif
+
+  ! To avoid .999999 numbers
+  binom_func = binom_func + 0.5d0
+
 end
 
 

commit 7df328724152480842fde2a183b08cb0a81c116b

Fix bug introduced by 57527c94a27491107e

src/utils/util.irp.f
@@ -39,7 +39,7 @@ double precision function binom_func(i,j)
   endif
 
   ! To avoid .999999 numbers
-  binom_func = binom_func + 0.5d0
+  binom_func = floor(binom_func + 0.5d0)
 
 end
 