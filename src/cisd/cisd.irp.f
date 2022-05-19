f4b5cf7abf5476c517eacf8e9c2018d5e346e442
ydamour <yann.damour@hotmail.fr>
2022-05-19 10:28:05 +0200

    update au to eV

--- src/cisd/cisd.irp.f
+++ src/cisd/cisd.irp.f
@@ -120,8 +120,8 @@ subroutine run
     print*,'******************************'
     print*,'Excitation energies (eV)    (CISD+Q)'
     do i = 2, N_states
-      print*, i ,(CI_energy(i) - CI_energy(1))/0.0367502d0, &
-        (cisdq(i) - cisdq(1)) / 0.0367502d0
+      print*, i ,(CI_energy(i) - CI_energy(1))*27.211396641308d0, &
+        (cisdq(i) - cisdq(1)) * 27.211396641308d0
     enddo
   endif
 
