7a67d69c0a794bbb0e6cbe24a5b9a62142479f44
ydamour <yann.damour@hotmail.fr>
2022-05-19 11:12:20 +0200

    au to Debye

--- src/utils/units.irp.f
+++ src/utils/units.irp.f
@@ -8,3 +8,15 @@ BEGIN_PROVIDER [double precision, ha_to_ev]
   ha_to_ev = 27.211396641308d0
 
 END_PROVIDER
+
+BEGIN_PROVIDER [double precision, au_to_D]
+
+  implicit none
+  BEGIN_DOC
+  ! Converstion from au to Debye
+  END_DOC
+
+  au_to_D = 2.5415802529d0
+
+END_PROVIDER
+
