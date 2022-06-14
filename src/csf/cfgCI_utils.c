da9f676513b38dc2d48b0bf97cdb10763fda1299
v1j4y <vijay.gopal.c@gmail.com>
2022-06-14 11:34:37 +0200

    Fixed typo.

--- src/csf/cfgCI_utils.c
+++ src/csf/cfgCI_utils.c
@@ -1338,10 +1338,10 @@ void getbftodetfunction(Tree *dettree, int NSOMO, int MS, int *BF1, double *rowv
     int phase_cfg_to_qp=1;
     int addr = -1;
     for(int i = 0; i < npairs; i++){
-        //for(int j = 0; j < NSOMO; j++) {
-        //    inpdet[j] = detslist[i*NSOMO + j];
-        //    printf(" %d ",inpdet[j]);
-        //}
+        for(int j = 0; j < NSOMO; j++) {
+            inpdet[j] = detslist[i*NSOMO + j];
+            //printf(" %d ",inpdet[j]);
+        }
         //printf("\n");
         findAddofDetDriver(dettree, NSOMO, inpdet, &addr);
         //printf("(%d) - addr  = %d\n",i,addr);
