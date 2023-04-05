5aed62450e000fa8d1840f4287559c2b314241a7
AbdAmmar <abd.ammar.phys@gmail.com>
2023-04-05 15:59:38 +0200

    print tc energy: OK

--- src/tc_bi_ortho/tc_bi_ortho.irp.f
+++ src/tc_bi_ortho/tc_bi_ortho.irp.f
@@ -89,36 +89,3 @@ end
 
 
 
-subroutine write_tc_energy()
-
-  implicit none
-  integer          :: i, j, k
-  double precision :: hmono, htwoe, htot
-  double precision :: E_TC, O_TC
-
-  do k = 1, n_states
-
-    E_TC = 0.d0
-    do i = 1, N_det
-      do j = 1, N_det
-        htot = htilde_matrix_elmt_bi_ortho(j,i)
-        !call hmat_bi_ortho(psi_det(1,1,i), psi_det(1,1,j), N_int, hmono, htwoe, htot)
-        E_TC = E_TC + psi_l_coef_bi_ortho(i,k) * psi_r_coef_bi_ortho(j,k) * htot
-        !E_TC = E_TC + leigvec_tc_bi_orth(i,k) * reigvec_tc_bi_orth(j,k) * htot
-      enddo
-    enddo
-
-    O_TC = 0.d0
-    do i = 1, N_det
-      O_TC = O_TC + psi_l_coef_bi_ortho(i,k) * psi_r_coef_bi_ortho(i,k)
-      !O_TC = O_TC + leigvec_tc_bi_orth(i,k) * reigvec_tc_bi_orth(i,k)
-    enddo
-
-    print *, ' state :', k
-    print *, " E_TC = ", E_TC
-    print *, " O_TC = ", O_TC
-
-  enddo
-
-end
-
