b2e65d010be82ec5cab814013e50565e7fa7df47
AbdAmmar <abd.ammar.phys@gmail.com>
2023-04-27 16:52:31 +0200

    added OPENMP for 3e terms

--- src/tc_scf/fock_3e_bi_ortho_uhf.irp.f
+++ src/tc_scf/fock_3e_bi_ortho_uhf.irp.f
@@ -65,8 +65,8 @@ BEGIN_PROVIDER [double precision, fock_3e_uhf_mo_a, (mo_num, mo_num)]
   PROVIDE mo_l_coef mo_r_coef
   PROVIDE fock_3e_uhf_mo_cs
 
-  !print *, ' PROVIDING fock_3e_uhf_mo_a ...'
-  !call wall_time(ti)
+  print *, ' Providing fock_3e_uhf_mo_a ...'
+  call wall_time(ti)
 
   o = elec_beta_num + 1
 
@@ -146,8 +146,8 @@ BEGIN_PROVIDER [double precision, fock_3e_uhf_mo_a, (mo_num, mo_num)]
     enddo
   enddo
 
-  !call wall_time(tf)
-  !print *, ' total Wall time for fock_3e_uhf_mo_a =', tf - ti
+  call wall_time(tf)
+  print *, ' Wall time for fock_3e_uhf_mo_a =', tf - ti
 
 END_PROVIDER 
 
