541d7f5ff91b246bd9454d4f14879ca54470e837
eginer <giner.emmanuel@gmail.com>
2023-10-03 20:04:34 +0200

    added attachment orbitals

--- src/tc_bi_ortho/tc_bi_ortho_prop.irp.f
+++ src/tc_bi_ortho/tc_bi_ortho_prop.irp.f
@@ -34,4 +34,19 @@ subroutine test
  do i= 1, 3
   print*,tc_bi_ortho_dipole(i,1)
  enddo
+ integer, allocatable           :: occ(:,:)
+ integer                        :: n_occ_ab(2)
+ allocate(occ(N_int*bit_kind_size,2))
+ call bitstring_to_list_ab(ref_bitmask, occ, n_occ_ab, N_int)
+ integer :: ispin,j,jorb
+ double precision :: accu
+ accu = 0.d0
+ do ispin=1, 2
+  do i = 1, n_occ_ab(ispin)
+   jorb = occ(i,ispin)
+   accu += mo_bi_orth_bipole_z(jorb,jorb)
+  enddo
+ enddo
+ print*,'accu = ',accu
+ 
 end
