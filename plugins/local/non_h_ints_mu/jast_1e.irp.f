9b2ba694d9e7f71801c5dac7c8073c06d8605b47
AbdAmmar <abd.ammar.phys@gmail.com>
2024-01-24 19:25:17 +0100

    Improved AosxAos representations of 1e-Jastrow

--- plugins/local/non_h_ints_mu/jast_1e.irp.f
+++ plugins/local/non_h_ints_mu/jast_1e.irp.f
@@ -231,96 +231,22 @@ END_PROVIDER
 !    !$OMP END PARALLEL
 !
 !    deallocate(coef_fit)
-!
-!  elseif(j1e_type .eq. "Charge_Harmonizer_AO2") then
-!
-!    ! \grad_1 \sum_{\eta,\beta} C_{\eta,\beta} \chi_{\eta} \chi_{\beta}
-!    ! where 
-!    !       \chi_{\eta} are the AOs
-!    !       C_{\eta,\beta} are fitted to mimic (j1e_type .eq. "Charge_Harmonizer")
-!    !
-!    ! The - sign is in the parameters C_{\eta,\beta}
-!
-!    PROVIDE aos_grad_in_r_array
-!
-!    allocate(coef_fit2(ao_num*ao_num))
-!
-!    if(mpi_master) then
-!      call ezfio_has_jastrow_j1e_coef_ao2(exists)
-!    endif 
-!    IRP_IF MPI_DEBUG
-!      print *,  irp_here, mpi_rank
-!      call MPI_BARRIER(MPI_COMM_WORLD, ierr)
-!    IRP_ENDIF
-!    IRP_IF MPI
-!      call MPI_BCAST(coef_fit2, ao_num*ao_num, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
-!      if (ierr /= MPI_SUCCESS) then
-!        stop 'Unable to read j1e_coef_ao2 with MPI'
-!      endif
-!    IRP_ENDIF
-!    if(exists) then
-!      if(mpi_master) then
-!        write(6,'(A)') '.. >>>>> [ IO READ: j1e_coef_ao2 ] <<<<< ..'
-!        call ezfio_get_jastrow_j1e_coef_ao2(coef_fit2)
-!        IRP_IF MPI
-!          call MPI_BCAST(coef_fit2, ao_num*ao_num, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
-!          if (ierr /= MPI_SUCCESS) then
-!            stop 'Unable to read j1e_coef_ao2 with MPI'
-!          endif
-!        IRP_ENDIF
-!      endif
-!    else
-!
-!      call get_j1e_coef_fit_ao2(ao_num*ao_num, coef_fit2)
-!      call ezfio_set_jastrow_j1e_coef_ao2(coef_fit2)
-!
-!    endif
-!
-!    !$OMP PARALLEL                                &
-!    !$OMP DEFAULT (NONE)                          &
-!    !$OMP PRIVATE (i, j, ij, ipoint, c)           &
-!    !$OMP SHARED (n_points_final_grid, ao_num,    &
-!    !$OMP         aos_grad_in_r_array, coef_fit2, &
-!    !$OMP         aos_in_r_array, j1e_gradx, j1e_grady, j1e_gradz)
-!    !$OMP DO SCHEDULE (static)
-!    do ipoint = 1, n_points_final_grid
-!
-!      j1e_gradx(ipoint) = 0.d0
-!      j1e_grady(ipoint) = 0.d0
-!      j1e_gradz(ipoint) = 0.d0
-!
-!      do i = 1, ao_num
-!        do j = 1, ao_num
-!          ij = (i-1)*ao_num + j
-!
-!          c = coef_fit2(ij)
-!
-!          j1e_gradx(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,1) + aos_grad_in_r_array(i,ipoint,1) * aos_in_r_array(j,ipoint))
-!          j1e_grady(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,2) + aos_grad_in_r_array(i,ipoint,2) * aos_in_r_array(j,ipoint))
-!          j1e_gradz(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,3) + aos_grad_in_r_array(i,ipoint,3) * aos_in_r_array(j,ipoint))
-!        enddo
-!      enddo
-!    enddo
-!    !$OMP END DO
-!    !$OMP END PARALLEL
-!
-!    deallocate(coef_fit2)
 
   elseif(j1e_type .eq. "Charge_Harmonizer_AO") then
 
-    ! \sum_{\eta} \vec{C}_{\eta} \chi_{\eta}
+    ! \grad_1 \sum_{\eta,\beta} C_{\eta,\beta} \chi_{\eta} \chi_{\beta}
     ! where 
     !       \chi_{\eta} are the AOs
-    !       \vec{C}_{\eta} are fitted to mimic (j1e_type .eq. "Charge_Harmonizer")
+    !       C_{\eta,\beta} are fitted to mimic (j1e_type .eq. "Charge_Harmonizer")
     !
-    ! The - sign is in the parameters \vec{C}_{\eta}
+    ! The - sign is in the parameters C_{\eta,\beta}
 
     PROVIDE aos_grad_in_r_array
 
-    allocate(coef_fit3(ao_num,3))
+    allocate(coef_fit2(ao_num*ao_num))
 
     if(mpi_master) then
-      call ezfio_has_jastrow_j1e_coef_ao3(exists)
+      call ezfio_has_jastrow_j1e_coef_ao2(exists)
     endif 
     IRP_IF MPI_DEBUG
       print *,  irp_here, mpi_rank
@@ -328,34 +254,34 @@ END_PROVIDER
     IRP_ENDIF
     IRP_IF MPI
       include 'mpif.h'
-      call MPI_BCAST(coef_fit3, (ao_num*3), MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+      call MPI_BCAST(coef_fit2, ao_num*ao_num, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
       if (ierr /= MPI_SUCCESS) then
-        stop 'Unable to read j1e_coef_ao3 with MPI'
+        stop 'Unable to read j1e_coef_ao2 with MPI'
       endif
     IRP_ENDIF
     if(exists) then
       if(mpi_master) then
-        write(6,'(A)') '.. >>>>> [ IO READ: j1e_coef_ao3 ] <<<<< ..'
-        call ezfio_get_jastrow_j1e_coef_ao3(coef_fit3)
+        write(6,'(A)') '.. >>>>> [ IO READ: j1e_coef_ao2 ] <<<<< ..'
+        call ezfio_get_jastrow_j1e_coef_ao2(coef_fit2)
         IRP_IF MPI
-          call MPI_BCAST(coef_fit3, (ao_num*3), MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+          call MPI_BCAST(coef_fit2, ao_num*ao_num, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
           if (ierr /= MPI_SUCCESS) then
-            stop 'Unable to read j1e_coef_ao3 with MPI'
+            stop 'Unable to read j1e_coef_ao2 with MPI'
           endif
         IRP_ENDIF
       endif
     else
 
-      call get_j1e_coef_fit_ao3(ao_num, coef_fit3)
-      call ezfio_set_jastrow_j1e_coef_ao3(coef_fit3)
+      call get_j1e_coef_fit_ao2(ao_num*ao_num, coef_fit2)
+      call ezfio_set_jastrow_j1e_coef_ao2(coef_fit2)
 
     endif
 
     !$OMP PARALLEL                                &
     !$OMP DEFAULT (NONE)                          &
-    !$OMP PRIVATE (i, ipoint, cx, cy, cz)         &
+    !$OMP PRIVATE (i, j, ij, ipoint, c)           &
     !$OMP SHARED (n_points_final_grid, ao_num,    &
-    !$OMP         aos_grad_in_r_array, coef_fit3, &
+    !$OMP         aos_grad_in_r_array, coef_fit2, &
     !$OMP         aos_in_r_array, j1e_gradx, j1e_grady, j1e_gradz)
     !$OMP DO SCHEDULE (static)
     do ipoint = 1, n_points_final_grid
@@ -363,20 +289,95 @@ END_PROVIDER
       j1e_gradx(ipoint) = 0.d0
       j1e_grady(ipoint) = 0.d0
       j1e_gradz(ipoint) = 0.d0
-      do i = 1, ao_num
-        cx = coef_fit3(i,1)
-        cy = coef_fit3(i,2)
-        cz = coef_fit3(i,3)
 
-        j1e_gradx(ipoint) += cx * aos_in_r_array(i,ipoint)
-        j1e_grady(ipoint) += cy * aos_in_r_array(i,ipoint)
-        j1e_gradz(ipoint) += cz * aos_in_r_array(i,ipoint)
+      do i = 1, ao_num
+        do j = 1, ao_num
+          ij = (i-1)*ao_num + j
+
+          c = coef_fit2(ij)
+
+          j1e_gradx(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,1) + aos_grad_in_r_array(i,ipoint,1) * aos_in_r_array(j,ipoint))
+          j1e_grady(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,2) + aos_grad_in_r_array(i,ipoint,2) * aos_in_r_array(j,ipoint))
+          j1e_gradz(ipoint) += c * (aos_in_r_array(i,ipoint) * aos_grad_in_r_array(j,ipoint,3) + aos_grad_in_r_array(i,ipoint,3) * aos_in_r_array(j,ipoint))
+        enddo
       enddo
     enddo
     !$OMP END DO
     !$OMP END PARALLEL
 
-    deallocate(coef_fit3)
+    deallocate(coef_fit2)
+
+!  elseif(j1e_type .eq. "Charge_Harmonizer_AO3") then
+!
+!    ! \sum_{\eta} \vec{C}_{\eta} \chi_{\eta}
+!    ! where 
+!    !       \chi_{\eta} are the AOs
+!    !       \vec{C}_{\eta} are fitted to mimic (j1e_type .eq. "Charge_Harmonizer")
+!    !
+!    ! The - sign is in the parameters \vec{C}_{\eta}
+!
+!    PROVIDE aos_grad_in_r_array
+!
+!    allocate(coef_fit3(ao_num,3))
+!
+!    if(mpi_master) then
+!      call ezfio_has_jastrow_j1e_coef_ao3(exists)
+!    endif 
+!    IRP_IF MPI_DEBUG
+!      print *,  irp_here, mpi_rank
+!      call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+!    IRP_ENDIF
+!    IRP_IF MPI
+!      !include 'mpif.h'
+!      call MPI_BCAST(coef_fit3, (ao_num*3), MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+!      if (ierr /= MPI_SUCCESS) then
+!        stop 'Unable to read j1e_coef_ao3 with MPI'
+!      endif
+!    IRP_ENDIF
+!    if(exists) then
+!      if(mpi_master) then
+!        write(6,'(A)') '.. >>>>> [ IO READ: j1e_coef_ao3 ] <<<<< ..'
+!        call ezfio_get_jastrow_j1e_coef_ao3(coef_fit3)
+!        IRP_IF MPI
+!          call MPI_BCAST(coef_fit3, (ao_num*3), MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+!          if (ierr /= MPI_SUCCESS) then
+!            stop 'Unable to read j1e_coef_ao3 with MPI'
+!          endif
+!        IRP_ENDIF
+!      endif
+!    else
+!
+!      call get_j1e_coef_fit_ao3(ao_num, coef_fit3)
+!      call ezfio_set_jastrow_j1e_coef_ao3(coef_fit3)
+!
+!    endif
+!
+!    !$OMP PARALLEL                                &
+!    !$OMP DEFAULT (NONE)                          &
+!    !$OMP PRIVATE (i, ipoint, cx, cy, cz)         &
+!    !$OMP SHARED (n_points_final_grid, ao_num,    &
+!    !$OMP         aos_grad_in_r_array, coef_fit3, &
+!    !$OMP         aos_in_r_array, j1e_gradx, j1e_grady, j1e_gradz)
+!    !$OMP DO SCHEDULE (static)
+!    do ipoint = 1, n_points_final_grid
+!
+!      j1e_gradx(ipoint) = 0.d0
+!      j1e_grady(ipoint) = 0.d0
+!      j1e_gradz(ipoint) = 0.d0
+!      do i = 1, ao_num
+!        cx = coef_fit3(i,1)
+!        cy = coef_fit3(i,2)
+!        cz = coef_fit3(i,3)
+!
+!        j1e_gradx(ipoint) += cx * aos_in_r_array(i,ipoint)
+!        j1e_grady(ipoint) += cy * aos_in_r_array(i,ipoint)
+!        j1e_gradz(ipoint) += cz * aos_in_r_array(i,ipoint)
+!      enddo
+!    enddo
+!    !$OMP END DO
+!    !$OMP END PARALLEL
+!
+!    deallocate(coef_fit3)
 
   else
 
