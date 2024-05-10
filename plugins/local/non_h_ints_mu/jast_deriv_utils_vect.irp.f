812e75982b96959485b8f7ccd333ef3a1b1b570b
eginer <giner.emmanuel@gmail.com>
2024-05-10 17:23:51 +0200

    minor modifs in plugins/local/non_h_ints_mu/jast_deriv_utils_vect.irp.f

--- plugins/local/non_h_ints_mu/jast_deriv_utils_vect.irp.f
+++ plugins/local/non_h_ints_mu/jast_deriv_utils_vect.irp.f
@@ -867,19 +867,20 @@ subroutine jBH_elem_fct_grad(alpha, r1, r2, fct, grad1_fct)
               + (r1(2) - r2(2)) * (r1(2) - r2(2)) &
               + (r1(3) - r2(3)) * (r1(3) - r2(3)) )
 
-  tmp1 = 1.d0 / (1.d0 + alpha * dist)
 
-  fct = alpha * dist * tmp1
-
-  if(dist .lt. 1d-10) then
-    grad1_fct(1) = 0.d0
-    grad1_fct(2) = 0.d0
-    grad1_fct(3) = 0.d0
-  else
+  if(dist .ge. 1d-10) then
+    tmp1 = 1.d0 / (1.d0 + alpha * dist)
+    
+    fct = alpha * dist * tmp1
     tmp2 = alpha * tmp1 * tmp1 / dist
     grad1_fct(1) = tmp2 * (r1(1) - r2(1))
     grad1_fct(2) = tmp2 * (r1(2) - r2(2))
     grad1_fct(3) = tmp2 * (r1(3) - r2(3))
+  else
+    grad1_fct(1) = 0.d0
+    grad1_fct(2) = 0.d0
+    grad1_fct(3) = 0.d0
+    fct = 0.d0
   endif
 
   return
