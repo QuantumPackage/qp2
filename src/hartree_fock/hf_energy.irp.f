682c408ae941439311109043bb6e350019561d1f
Ydrnan <yann.damour@hotmail.fr>
2024-09-27 16:07:25 +0200

    Set restore_symmetry and nullify to 10^-15

--- src/hartree_fock/hf_energy.irp.f
+++ src/hartree_fock/hf_energy.irp.f
@@ -36,3 +36,24 @@ END_PROVIDER
  HF_energy += nuclear_repulsion
 END_PROVIDER
 
+
+ BEGIN_PROVIDER [ double precision, HF_kinetic_energy]
+&BEGIN_PROVIDER [ double precision, HF_n_e_energy]
+ implicit none
+ BEGIN_DOC
+ ! Hartree-Fock energy containing the nuclear repulsion, and its one- and two-body components.
+ END_DOC
+ integer :: i,j
+ double precision :: tmp1, tmp2
+ HF_n_e_energy = 0.d0
+ HF_kinetic_energy = 0.d0
+ do j=1,ao_num
+   do i=1,ao_num
+    tmp1 = ao_integrals_n_e(i,j) * (SCF_density_matrix_ao_alpha(i,j) + SCF_density_matrix_ao_beta (i,j) )
+    tmp2 = ao_kinetic_integrals(i,j) * (SCF_density_matrix_ao_alpha(i,j) + SCF_density_matrix_ao_beta (i,j) )
+    HF_n_e_energy += tmp1
+    HF_kinetic_energy += tmp2
+   enddo
+ enddo
+END_PROVIDER
+
