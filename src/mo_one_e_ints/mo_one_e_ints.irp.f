c35278cddbe72e988f671587af4c480714ddd70f
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2024-11-26 16:55:57 +0100

    Moved use_pw into ao_basis module, and ao_one_e_integrals_from_mo in mo_one_e_ints

--- src/mo_one_e_ints/mo_one_e_ints.irp.f
+++ src/mo_one_e_ints/mo_one_e_ints.irp.f
@@ -21,3 +21,14 @@ BEGIN_PROVIDER [ double precision, mo_one_e_integrals,(mo_num,mo_num)]
   call nullify_small_elements(mo_num,mo_num,mo_one_e_integrals,size(mo_one_e_integrals,1),1.d-15)
 
 END_PROVIDER
+
+
+BEGIN_PROVIDER [ double precision, ao_one_e_integrals_from_mo, (ao_num, ao_num)]
+ implicit none
+ BEGIN_DOC
+! Integrals of the one e hamiltonian obtained from the integrals on the MO basis 
+!
+! WARNING : this is equal to ao_one_e_integrals only if the AO and MO basis have the same number of functions
+ END_DOC
+ call mo_to_ao(mo_one_e_integrals,mo_num,ao_one_e_integrals_from_mo,ao_num)
+END_PROVIDER 
