c285aaca49d26ed4f3019aa2b9adc900dbc9e443
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2025-01-31 17:14:49 +0100

    Stabilize PT2 when expected accuracy is low

--- src/cipsi_utils/pt2_stoch_routines.irp.f
+++ src/cipsi_utils/pt2_stoch_routines.irp.f
@@ -530,7 +530,7 @@ subroutine pt2_collector(zmq_socket_pull, E, relative_error, pt2_data, pt2_data_
         avg  = E0 + pt2_data_S(t) % pt2(pt2_stoch_istate) / dble(c)
         avg2 = v0 + pt2_data_S(t) % variance(pt2_stoch_istate) / dble(c)
         avg3(:) = n0(:) + pt2_data_S(t) % overlap(:,pt2_stoch_istate) / dble(c)
-        if ((avg /= 0.d0) .or. (n == N_det_generators) ) then
+        if (((c>=10).and.(avg /= 0.d0)) .or. (n == N_det_generators) ) then
           do_exit = .true.
         endif
         if (qp_stop()) then
